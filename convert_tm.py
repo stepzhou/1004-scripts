d79a2fbe91f5e835e3b92e9b35543e6408df9050
Stephen Zhou <step.zhou@gmail.com>
2013-05-10 23:49:51 -0400

    fixing if starts with 1

--- convert_tm.py
+++ convert_tm.py
@@ -4,7 +4,7 @@ import sys
 def convert(tm_string):
     # replace state 1 with starting state 0
     tm_string = re.sub(r'(\d+ \w+ \w+) 1 ([rl])', r'\1 0 \2', tm_string)
-    tm_string = re.sub(r'1 ([^ ] [^ ] [^ ] [^ ])', r'0 \1', tm_string)
+    tm_string = re.sub(r'1 (\w+ \w+ \w+ \w+)', r'0 \1', tm_string)
     # switch last 2 elements
     tm_string = re.sub(r'(\d+) ([rl])', r'\2 \1', tm_string)
     # replace b with _
